27b591a21dd300685dbcf4875e25da0013369284
MadMaxINDIAN <naman22khater@gmail.com>
2022-10-12 11:25:55 +0530

    backend integrated from aws

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,4 +1,4 @@
-export const BASE_URL_1 = 'https://kalawati-finance-company.herokuapp.com'
+export const BASE_URL_1 = 'http://ec2-65-2-11-185.ap-south-1.compute.amazonaws.com:5000'
 
 // https://kalawati-finance-company.herokuapp.com
 // http://localhost:5000

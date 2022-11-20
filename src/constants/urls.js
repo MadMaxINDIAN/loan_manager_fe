d21187bf728b2140b4d2d79cfcfd8f5c3a33c75f
Samyak Shah <samyakshah123@gmail.com>
2022-11-20 22:53:38 +0530

    Build

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,4 +1,5 @@
-export const BASE_URL_1 = 'https://kalawati-finance-company.herokuapp.com'
+// export const BASE_URL_1 = 'http://localhost:5000'
+export const BASE_URL_1 = 'http://ec2-3-110-160-107.ap-south-1.compute.amazonaws.com:5000'
 
 // https://kalawati-finance-company.herokuapp.com
 // http://localhost:5000

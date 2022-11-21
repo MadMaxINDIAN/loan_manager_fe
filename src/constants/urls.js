601eb894c177a4c353c564f4f4b25ea06e289db3
Samyak Shah <samyakshah123@gmail.com>
2022-11-21 14:49:38 +0530

    Build

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,5 +1,5 @@
 // export const BASE_URL_1 = 'http://localhost:5000'
-export const BASE_URL_1 = 'http://ec2-3-110-160-107.ap-south-1.compute.amazonaws.com:5000'
+export const BASE_URL_1 = 'https://backend.laxmikalafinance.info/'
 
 // https://kalawati-finance-company.herokuapp.com
 // http://localhost:5000
